e576e4fffaca923850d9cc67e9294511196c7fb7
treemmett <treemmett@gmail.com>
2018-03-12 11:37:59 -0600

    Adds scan for host, asset, and username

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('done');
+const os = require('os');
+
+//Gather all usable information
+const data = {
+  hostname: os.hostname(),
+  asset: os.hostname().match(/\d+/)[0],
+  username: os.homedir().split('\\')[os.homedir().split('\\').length - 1]
+}
+
+console.log(data);
