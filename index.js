774014b9b21d4bbd7e84237326b2e0726f23a7c9
treemmett <treemmett@gmail.com>
2018-03-12 11:41:21 -0600

    Adds MAC address scan

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const os = require('os');
 const data = {
   hostname: os.hostname(),
   asset: os.hostname().match(/\d+/)[0],
-  username: os.homedir().split('\\')[os.homedir().split('\\').length - 1]
+  username: os.homedir().split('\\')[os.homedir().split('\\').length - 1],
+  network: {}
+}
+
+//Gather MAC's
+const nic = os.networkInterfaces();
+for(let i in nic){
+  //Don't include VirtualBox or Loopbacks
+  if(i.match(/virtualbox/gi) || i.match(/loopback/gi)){
+    continue;
+  }
+
+  data.network[i] = nic[i][0].mac;
 }
 
 console.log(data);
