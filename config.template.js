d3c4180fdba5fd98f0dc68f40414f7d7125e3e0b
treemmett <treemmett@gmail.com>
2018-03-16 17:14:25 -0600

    Removes Active Directory search to get full name

--- config.template.js
+++ config.template.js
@@ -1,10 +1,4 @@
 module.exports = {
   xlsxLocation: '\\\\path\\to\\template.xlsx',
   jsonLocation: '\\\\path\\to\\json\\folder\\'
-  ad: {
-    url: 'ldap://my.domain.com',
-    baseDN: 'dc=my,dc=domain,dc=com',
-    username: 'admin@my.domain.com',
-    password: 'password'
-  }
 }
--- getname.js
+++ getname.js
@@ -1,29 +1,50 @@
-const ActiveDirectory = require('activedirectory');
 const config = require('./config');
+const exec = require('child_process').exec;
 
 module.exports = function(username){
-  const name = {
-    first: null,
-    last: null
-  }
-
   return new Promise((resolve, reject) => {
     if(process.platform === 'win32'){
-      const ad = new ActiveDirectory(config.ad);
-      ad.findUser(username, function(err, user){
+      exec('net user %username% /domain | find /i "full name"', {cwd: 'C:\\Windows'}, (err, stdout, stderr) => {
         if(err){
           reject(err);
         }
 
-        if(user){
-          name.first = user.givenName;
-          name.last = user.sn;
+        if(stderr){
+          reject(stderr);
         }
 
-        resolve(name);
+        //Remove other characters from out
+        const nameString = stdout.split(/\s{2,}/g)[1];
+
+        resolve(parseName(name));
       });
     }else{
-      resolve(name);
+      resolve(parseName());
     }
   });
+}
+
+function parseName(name){
+  const output = {
+    first: null,
+    last: null
+  }
+
+  if(!name){
+    return output;
+  }
+
+  //Check if output is in "First Last" or "Last, First" format
+  const lF = name.match(/(\S+), (\S+)/);
+  const fL = name.match(/(\S+) (\S+)/);
+
+  if(lF){
+    output.last = lF[1];
+    output.first = lF[2];
+  }else if(fL){
+    output.first = fL[1];
+    output.last = fL[2];
+  }
+
+  return output;
 }
