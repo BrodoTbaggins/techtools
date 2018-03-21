2644c1dc84b4aa0ad7f78d81033789b8cfc71a9b
treemmett <treemmett@gmail.com>
2018-03-21 16:00:35 -0600

    Adds getAsset method for RedBeam

--- redbeam.js
+++ redbeam.js
@@ -104,6 +104,162 @@ const getUser = (firstName, lastName) => {
   });
 }
 
+const getAsset = assetTag => {
+  return new Promise((resolve, reject) => {
+    redBeam.then(session => {
+      session.get('https://webapi.assettracking.redbeam.com/api/Items?getby=asset&value='+assetTag)
+      .then(res => resolve(res.data))
+      .catch(err => {
+        if(err.response){
+          if(err.response.status === 404){
+            //Asset tag doesn't exist, create new
+            const data = {
+              id: '00000000-0000-0000-0000-000000000000',
+            	entityId: '',
+            	code: '',
+            	description: '',
+            	serialNumber: '',
+            	manufacturerId: 'ad347b99-2b38-42d7-8aaf-80f2e21237b1',
+            	vendorId: '8e86efee-420d-4de6-8251-df404b198289',
+            	locationId: '38013da6-06ef-4ff2-9472-7b92845da927',
+            	itemTypeId: '1a3fc08d-e433-48d7-8e94-6758f7e13504',
+            	statusId: '5eb257a2-f2bf-402e-aada-2755d343ec7f',
+            	personId: '2269e681-0208-4427-ba0d-1fc039bbbd37',
+            	firstName: '',
+            	lastName: '',
+            	accountId: '158470c9-5a8d-4238-994a-3e6c0c3e9601',
+            	departmentId: '74e8cdf1-0a57-4214-b4ad-deea40b0fe9d',
+            	model: '',
+            	poNumber: '',
+            	cost: 0,
+              warrantyNumber: '',
+            	leaseNumber: '',
+            	notes: '',
+            	quantity: 0,
+              inactive: false,
+              warrantyStartDate: null,
+              warrantyEndDate: null,
+              leaseStartDate: null,
+              leaseEndDate: null,
+              acquisitionDate: null,
+              dateCreated: null,
+              changedAt: null,
+              effectiveStartDate: null,
+              effectiveEndDate: null,
+              tenantId: '',
+            	editingUserName: '',
+            	userName: '',
+            	userField1: '',
+            	userField2: '',
+            	userField3: '',
+            	userField4: '',
+            	userField5: '',
+            	userField6: '',
+            	userField7: '',
+            	userField8: '',
+            	userField9: '',
+            	userField10: '',
+            	userField11: '',
+            	userField12: '',
+            	userField13: '',
+            	userField14: '',
+            	userField15: '',
+            	userField16: '',
+            	userField17: '',
+            	userField18: '',
+            	userField19: '',
+            	userField20: '',
+            	userField21: '',
+            	userField22: '',
+            	userField23: '',
+            	userField24: '',
+            	userField25: '',
+            	lamportTimestamp: '',
+            	itcEvent: '0',
+            	itcId: '1',
+            	target: {
+                id: '',
+              	entityId: '',
+              	code: '',
+              	description: '',
+              	serialNumber: '',
+              	manufacturerId: '',
+              	vendorId: '',
+              	locationId: '',
+              	itemTypeId: '',
+              	statusId: '',
+              	personId: '',
+              	firstName: '',
+              	lastName: '',
+              	accountId: '',
+              	departmentId: '',
+              	model: '',
+              	poNumber: '',
+              	cost: 0,
+                warrantyNumber: '',
+              	leaseNumber: '',
+              	notes: '',
+              	quantity: 0,
+                inactive: false,
+                warrantyStartDate: null,
+                warrantyEndDate: null,
+                leaseStartDate: null,
+                leaseEndDate: null,
+                acquisitionDate: null,
+                dateCreated: null,
+                changedAt: null,
+                effectiveStartDate: null,
+                effectiveEndDate: null,
+                tenantId: '',
+              	editingUserName: '',
+              	userName: '',
+              	userField1: '',
+              	userField2: '',
+              	userField3: '',
+              	userField4: '',
+              	userField5: '',
+              	userField6: '',
+              	userField7: '',
+              	userField8: '',
+              	userField9: '',
+              	userField10: '',
+              	userField11: '',
+              	userField12: '',
+              	userField13: '',
+              	userField14: '',
+              	userField15: '',
+              	userField16: '',
+              	userField17: '',
+              	userField18: '',
+              	userField19: '',
+              	userField20: '',
+              	userField21: '',
+              	userField22: '',
+              	userField23: '',
+              	userField24: '',
+              	userField25: '',
+              	lamportTimestamp: '',
+              	itcEvent: '0',
+              	itcId: '1'
+              }
+            }
+            data.code = assetTag;
+
+            session.post('https://webapi.assettracking.redbeam.com/api/Items', data)
+            .then(res => resolve(res.data))
+            .catch(err => reject(err));
+          }else{
+            reject(err);
+          }
+        }else{
+          reject(err);
+        }
+      });
+    }).catch(err => reject(err));
+  });
+}
+
 module.exports = {
   getUser: getUser,
+  getAsset: getAsset
 }
