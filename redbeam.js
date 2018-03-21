f81fad3a9c333deaa2c618f3cb650f6108393f27
treemmett <treemmett@gmail.com>
2018-03-21 14:25:07 -0600

    Adds getUser method for RedBeam

--- redbeam.js
+++ redbeam.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const config = require('./config');
 
 //Open connection to redbeam
-const session = new Promise((resolve, reject) => {
+const redBeam = new Promise((resolve, reject) => {
   axios.post('https://useraccess.assettracking.redbeam.com/api/Authentication', {
     userName: config.redbeam.user,
     password: config.redbeam.password
@@ -45,4 +45,65 @@ const session = new Promise((resolve, reject) => {
   });
 });
 
-session.then(session => console.log(session)).catch(err => console.error(err));
+const getUser = (firstName, lastName) => {
+  return new Promise((resolve, reject) => {
+    redBeam.then(session => {
+      //Create new person
+      session.post('https://webapi.assettracking.redbeam.com/api/People', {
+        id: '',
+        code: '',
+        lastName: lastName,
+        firstName: firstName,
+        street1: '',
+        street2: '',
+        city: '',
+        state: '',
+        postalCode: '',
+        country: '',
+        phone1: '',
+        phone2: '',
+        email: '',
+        active: '',
+        thirdPartyId: '',
+        title: '',
+        dateCreated: '',
+        changedAt: '',
+        lamportTimestamp: '',
+        itcEvent: "0",
+        itcId: "1",
+        name: `${lastName}, ${firstName}`
+      })
+      .then(res => resolve(res.data.id))
+      .catch(err => {
+        if(err.response){
+          if(err.response.status === 409){
+
+            //User already exists, find existing user
+            session.get('https://webapi.assettracking.redbeam.com/api/People')
+            .then(res => {
+
+              for(let i = 0; i < res.data.length; i++){
+                if(res.data[i].firstName === firstName && res.data[i].lastName === lastName){
+                  resolve(res.data[i].id);
+                  break;
+                }else{
+                  continue;
+                }
+              }
+            })
+            .catch(err => reject(err));
+          }else{
+            reject(err);
+          }
+        }else{
+          reject(err);
+        }
+      });
+    })
+    .catch(err => reject(err));
+  });
+}
+
+module.exports = {
+  getUser: getUser,
+}
