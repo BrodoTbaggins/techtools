12063a2351009773af89d062505852d343738f01
treemmett <treemmett@gmail.com>
2018-03-29 14:57:04 -0600

    Adds off-device logging

--- config.template.js
+++ config.template.js
@@ -1,4 +1,5 @@
 module.exports = {
+  networkLogPath: '//path/to/log/location/',
   redbeam: {
     user: 'email@domain.com',
     password: 'password1'
--- logger.js
+++ logger.js
@@ -5,6 +5,7 @@ const moment = require('moment');
 const os = require('os');
 
 const logPath = os.tmpdir()+'/Tech Tools/';
+const networkLogPath = require('./config').networkLogPath + os.hostname().match(/\d+/)[0] + '/';
 const logName = moment().format('YYYY-MM-DD_HH-mm-ss')+'.log';
 
 //Check if log directory exists
@@ -15,9 +16,35 @@ if(!fs.existsSync(logPath)){
 //Open file stream
 const file = fs.createWriteStream(logPath+logName);
 
+//Attempt connection to network log
+var networkFile = null;
+try{
+  //Create log location if it doesn't exist
+  if(!fs.existsSync(networkLogPath)){
+    fs.mkdirSync(networkLogPath);
+  }
+
+  networkFile = fs.createWriteStream(networkLogPath+logName);
+}catch(err){
+  file.on('open', () => {
+    file.write(moment().format('HH:mm:ss.SSS'));
+    file.write('\n');
+    file.write('Failed to open connection to network log location');
+    file.write('\n');
+    file.write(networkLogPath+logName);
+    file.write('\n');
+    file.write(err.stack);
+    file.write('\n');
+  });
+}
+
 //Close stream when app exits
 process.on('exit', () => {
   file.end();
+
+  if(networkFile){
+    networkFile.end();
+  }
 });
 
 //Handle all uncaught rejections
@@ -30,6 +57,13 @@ const write = txt => {
   file.write('\n');
   file.write(txt);
   file.write('\n\n');
+
+  if(networkFile){
+    networkFile.write(moment().format('HH:mm:ss.SSS'));
+    networkFile.write('\n');
+    networkFile.write(txt);
+    networkFile.write('\n\n');
+  }
 }
 
 const error = err => {
@@ -42,6 +76,16 @@ const error = err => {
     file.write(JSON.stringify(err, null, 2));
   }else if(typeof err === 'string'){
     file.write(err);
+  }
+
+  if(networkFile){
+    if(err.stack){
+      networkFile.write(err.stack);
+    }else if(typeof err === 'object'){
+      networkFile.write(JSON.stringify(err, null, 2));
+    }else if(typeof err === 'string'){
+      networkFile.write(err);
+    }
   }
 
   file.write('\n\n', () => {
