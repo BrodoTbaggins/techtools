31d3795396de1c552a9c962e274b6fa897de4dc0
Eric Tessers <etessers@doterra.com>
2021-10-15 15:26:34 -0600

    Updated build.js file

--- build.js
+++ build.js
@@ -1,3 +1,3 @@
 const {exec} = require('pkg');
 
-exec(['index.js', '--targets', 'node8-win-x86,node8-macos-x64', '--output', 'build/tech_tools']);
+exec(['index.js', '--targets', 'node14-win-x64,node14-macos-x64', '--output', 'build/tech_tools']);
